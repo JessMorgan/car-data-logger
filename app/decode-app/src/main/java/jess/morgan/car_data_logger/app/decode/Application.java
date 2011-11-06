6494e3f377828984effa2761adf01ae98ac6b1d2
Jess Morgan <Jessica.Morgan.skis@gmail.com>
2011-11-06 00:15:03 -0400

    Added error checking to detect missing plugins / class name typos

--- app/decode-app/src/main/java/jess/morgan/car_data_logger/app/decode/Application.java
+++ app/decode-app/src/main/java/jess/morgan/car_data_logger/app/decode/Application.java
@@ -26,13 +26,16 @@ public class Application {
 		List<DataDecoder> decoders = new ArrayList<DataDecoder>();
 		for(String decoderClassName : config.getDecoders()) {
 			try {
-				decoders.add(
-						pluginManager.loadPlugin(
-								decoderClassName,
-								config.getDecoderConfig(decoderClassName),
-								DataDecoderFactory.class
-								)
+				DataDecoder decoder = pluginManager.loadPlugin(
+						decoderClassName,
+						config.getDecoderConfig(decoderClassName),
+						DataDecoderFactory.class
 						);
+				if(decoder == null) {
+					System.err.println("Decoder plugin not found: " + decoderClassName);
+					return;
+				}
+				decoders.add(decoder);
 			} catch (Exception e) {
 				System.err.println("Error loading decoder plugin: " + decoderClassName);
 				e.printStackTrace();
@@ -43,13 +46,16 @@ public class Application {
 		List<DataProcessor> dataProcessors = new ArrayList<DataProcessor>();
 		for(String dataProcessorClassName : config.getDataProcessors()) {
 			try {
-				dataProcessors.add(
-						pluginManager.loadPlugin(
-								dataProcessorClassName,
-								config.getDecoderConfig(dataProcessorClassName),
-								DataProcessorFactory.class
-								)
+				DataProcessor dataProcessor = pluginManager.loadPlugin(
+						dataProcessorClassName,
+						config.getDecoderConfig(dataProcessorClassName),
+						DataProcessorFactory.class
 						);
+				if(dataProcessor == null) {
+					System.err.println("Data processor plugin not found: " + dataProcessorClassName);
+					return;
+				}
+				dataProcessors.add(dataProcessor);
 			} catch (Exception e) {
 				System.err.println("Error loading data processor plugin: " + dataProcessorClassName);
 				e.printStackTrace();

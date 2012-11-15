eb45f7638707b113b1b07e5df88f759090701157
Jess Morgan <Jessica.Morgan.skis@gmail.com>
2012-11-14 22:48:28 -0500

    Added a video overlay gauge for displaying text

---
+++ video-overlay-gauges/text-gauge/src/main/java/jess/morgan/car_data_logger/video_overlay/gauge/text/TextGauge.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2012 Jess Morgan
+ *
+ * This file is part of car-data-logger.
+ *
+ * car-data-logger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * car-data-logger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with car-data-logger.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jess.morgan.car_data_logger.video_overlay.gauge.text;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.font.TextLayout;
+import java.awt.geom.Rectangle2D;
+import java.io.IOException;
+import java.util.Map;
+
+import jess.morgan.car_data_logger.video_overlay.gauge.Gauge;
+
+public class TextGauge implements Gauge {
+	private final String parameterName;
+	private final String format;
+	private final Color textColor;
+
+	public TextGauge(String parameterName, String format, Color textColor) throws IOException {
+		this.parameterName = parameterName;
+		this.format = (format == null ? "%s" : format);
+		this.textColor = (textColor == null ? Color.WHITE : textColor);
+	}
+
+	@Override
+	public void draw(Map<String, String> data, Graphics2D graphics, int x, int y, int width, int height) {
+		String str = data.get(parameterName);
+		Object obj = str;
+		try {
+			if(str != null && !str.isEmpty()) {
+				obj = Double.parseDouble(str);
+			}
+		} catch(NumberFormatException nfe) {
+			// Ignore - it's not a number
+		}
+		if(obj != str) {
+			str = String.format(format, obj);
+		}
+
+		TextLayout layout = new TextLayout(str, graphics.getFont(), graphics.getFontRenderContext());
+		Rectangle2D bounds = layout.getPixelBounds(null, x, y);
+
+		Color oldColor = graphics.getColor();
+		graphics.setColor(textColor);
+		layout.draw(graphics, x, (float) (y + bounds.getHeight()));
+		graphics.setColor(oldColor);
+	}
+}
---
+++ video-overlay-gauges/text-gauge/src/main/java/jess/morgan/car_data_logger/video_overlay/gauge/text/TextGaugeFactory.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2012 Jess Morgan
+ *
+ * This file is part of car-data-logger.
+ *
+ * car-data-logger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * car-data-logger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with car-data-logger.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jess.morgan.car_data_logger.video_overlay.gauge.text;
+
+import java.awt.Color;
+import java.util.Map;
+
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+import jess.morgan.car_data_logger.plugin.ConfigParameter;
+import jess.morgan.car_data_logger.video_overlay.gauge.Gauge;
+import jess.morgan.car_data_logger.video_overlay.gauge.GaugeFactory;
+
+@PluginImplementation
+public class TextGaugeFactory implements GaugeFactory {
+	@Override
+	public String getPluginDisplayName() {
+		return "GPS Track Gauge";
+	}
+
+	@Override
+	public ConfigParameter[] getConfigParameters() {
+		return new ConfigParameter[]{
+				new ConfigParameter("parameter",        "Parameter Name",                  String.class,  true),
+				new ConfigParameter("format",           "printf format",                   String.class,  false),
+				new ConfigParameter("textColor",        "Text Color",                      Color.class,   false),
+		};
+	}
+
+	@Override
+	public Gauge getPlugin(Map<String, Object> config) throws Exception {
+		String parameterName    = ConfigParameter.getParameter(config, "parameter",      String.class,   true);
+		String format           = ConfigParameter.getParameter(config, "format",         String.class, false);
+		Color textColor         = ConfigParameter.getParameter(config, "textColor",      Color.class,  false);
+
+		return new TextGauge(parameterName, format, textColor);
+	}
+}
